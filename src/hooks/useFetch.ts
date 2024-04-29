868d6f38f23f1d9bfc8bf31b813c267dd9941f6f
Gabecker977 <gabriel.augusto.becker@gmail.com>
2024-04-29 13:57:52 -0300

    FirstCommit

--- src/hooks/useFetch.ts
+++ src/hooks/useFetch.ts
@@ -2,7 +2,7 @@ import useSWR from 'swr'
 import api from '../services/api';
 
 export function useFetch<Data = any, Error = any>(url: string) {
-  const { data, error, mutate } = useSWR<Data, Error>(url, async url => {
+  const { data, error, mutate } = useSWR<Data, Error>(url, async (url: string) => {
     const response = await api.get(url);
 
     return response.data;
